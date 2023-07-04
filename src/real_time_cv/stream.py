eafa5e2c289a354c594b6c5a7166db90ebfeff9a
plachert <pmlachert@gmail.com>
2023-07-04 18:29:36 +0200

    Loop the video

---
+++ src/real_time_cv/stream.py
@@ -0,0 +1,25 @@
+import cv2
+from aiortc.mediastreams import VideoStreamTrack
+from av import VideoFrame
+
+
+class FromFileVideoStreamTrack(VideoStreamTrack):
+    """
+    Generate frames from video file.
+    """
+    def __init__(self, filename):
+        super().__init__()  # don't forget this!
+        self.filename = filename
+        self.cap = cv2.VideoCapture(filename)
+
+    async def recv(self):
+        pts, time_base = await self.next_timestamp()
+        print(pts)
+        _, frame = self.cap.read()
+        if frame is None:
+            self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)  # Reset frame position
+            _, frame = self.cap.read()
+        frame = VideoFrame.from_ndarray(frame, "bgr24")
+        frame.pts = pts
+        frame.time_base = time_base
+        return frame
