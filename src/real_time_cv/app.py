6511faee498c191ae69085293ee7d06368ff4963
plachert <pmlachert@gmail.com>
2023-07-04 19:28:01 +0200

    Add file uploader

--- src/real_time_cv/app.py
+++ src/real_time_cv/app.py
@@ -2,23 +2,37 @@ import cv2
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer
 from stream import FromFileVideoStreamTrack
+import tempfile
 
 
 # Main streamlit application
 def main():
     st.title("Video Stream from File")
+    method = st.sidebar.radio('Select stream', options=['Webcam', 'File'])
 
     # Select video file
-    video_file = "/home/piotr/github/real_time_cv/test.mp4"
+    # video_file = "/home/piotr/github/real_time_cv/test.mp4"
 
-    if video_file is not None:
+
+    if method == "File":
         # Create video track from the selected file
-        video_track = FromFileVideoStreamTrack(video_file)#VideoFileTrack(video_file)
+        video_file = st.file_uploader("Upload a video file", type=["mp4", "avi"])
+        if video_file is not None:
+            temp_file = tempfile.NamedTemporaryFile(delete=False)
+            temp_file.write(video_file.read())
+            temp_file.close()
+            # Create video track from the saved file
+            video_track = FromFileVideoStreamTrack(temp_file.name)
 
-        # Create WebRTC streamer
+            # Create WebRTC streamer
+            webrtc_streamer(
+                key="video-file-stream",
+                source_video_track=video_track,
+                media_stream_constraints={"video": True, "audio": False},
+        )
+    else:
         webrtc_streamer(
-            key="video-file-stream",
-            source_video_track=video_track,
+            key="camera-stream",
             media_stream_constraints={"video": True, "audio": False},
         )
 
--- src/real_time_cv/stream.py
+++ src/real_time_cv/stream.py
@@ -8,13 +8,12 @@ class FromFileVideoStreamTrack(VideoStreamTrack):
     Generate frames from video file.
     """
     def __init__(self, filename):
-        super().__init__()  # don't forget this!
+        super().__init__()
         self.filename = filename
         self.cap = cv2.VideoCapture(filename)
 
     async def recv(self):
         pts, time_base = await self.next_timestamp()
-        print(pts)
         _, frame = self.cap.read()
         if frame is None:
             self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)  # Reset frame position
