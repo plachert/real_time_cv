d03d7c5d0609035c61feee232ddc936aec7ee7c7
plachert <pmlachert@gmail.com>
2023-07-05 11:18:39 +0200

    Fix processor not working for video stream. Add twilio ice

--- src/real_time_cv/app.py
+++ src/real_time_cv/app.py
@@ -1,34 +1,21 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
+from streamlit_webrtc import webrtc_streamer, WebRtcMode
 from stream import FromFileVideoStreamTrack, Dummy
 import tempfile
 import av
 import numpy as np
 import cv2
+from ice import get_ice_servers
+
+COMMON_RTC_CONFIG = {"iceServers": get_ice_servers()}
 
 def identity(frame: av.VideoFrame) -> av.VideoFrame:
-        image = frame.to_ndarray(format="bgr24")
         return frame
 
 def convert2gray(frame: av.VideoFrame) -> av.VideoFrame:
         image = frame.to_ndarray(format="bgr24")
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        print("RUN")
         return av.VideoFrame.from_ndarray(gray, format="gray")
-
-        # if model is None:
-        #     return image
-
-        # orig_h, orig_w = image.shape[0:2]
-
-        # # cv2.resize used in a forked thread may cause memory leaks
-        # input = np.asarray(Image.fromarray(image).resize((width, int(width * orig_h / orig_w))))
-
-        # transferred = style_transfer(input, model)
-
-        # result = Image.fromarray((transferred * 255).astype(np.uint8))
-        # image = np.asarray(result.resize((orig_w, orig_h)))
-        # return av.VideoFrame.from_ndarray(image, format="bgr24")
         
         
 available_processors = {
@@ -56,16 +43,19 @@ def main():
             # Create WebRTC streamer
             webrtc_streamer(
                 key="video-file-stream",
+                mode=WebRtcMode.RECVONLY,
                 source_video_track=video_track,
                 media_stream_constraints={"video": True, "audio": False},
                 video_frame_callback=processor,
+                rtc_configuration=COMMON_RTC_CONFIG,
         )
     else:
         webrtc_streamer(
             key="camera-stream",
-            source_video_track=Dummy(),
+            source_video_track=None,
             media_stream_constraints={"video": True, "audio": False},
             video_frame_callback=processor,
+            rtc_configuration=COMMON_RTC_CONFIG,
         )
 
 # Run the application
