748064cb2e8bdcca3550021a1544609fe62285c9
plachert <pmlachert@gmail.com>
2023-07-07 11:31:01 +0200

    Refactor, fix proessor synchronization

--- src/real_time_cv/app.py
+++ src/real_time_cv/app.py
@@ -7,8 +7,57 @@ import cv2
 import time
 import threading
 
-event = threading.Event()
+sync_event = threading.Event()
 
+def synchronize_processors(ref_processor, processor):
+    def ref_processor_wrapper(*args):
+        sync_event.wait()
+        result = ref_processor(*args)
+        sync_event.clear()
+        return result
+    def processor_wrapper(*args):
+        result = processor(*args)
+        sync_event.set()
+        return result
+    return ref_processor_wrapper, processor_wrapper
+
+
+def make_raw_stream(mode, source_video_track):
+    with st.sidebar:
+        ctx = webrtc_streamer(
+            key="stream-org",
+            mode=mode,
+            source_video_track=source_video_track,
+            desired_playing_state=DESIRED_PLAYING_STATE,
+            media_stream_constraints={"video": True, "audio": False},
+            rtc_configuration=ICE_CONFIG,
+            video_html_attrs={"hidden": True},
+            )
+    return ctx
+
+def make_processors_view(raw_stream, ref_processor, processor):
+    col_reference, col_processor = st.columns(2)
+    with col_reference:
+        webrtc_streamer(
+            key="stream-reference",
+            mode=WebRtcMode.RECVONLY,
+            video_frame_callback=ref_processor,
+            source_video_track=raw_stream.output_video_track,
+            desired_playing_state=raw_stream.state.playing,
+            rtc_configuration=ICE_CONFIG,
+            media_stream_constraints={"video": True, "audio": False},
+        )
+    with col_processor:
+        webrtc_streamer(
+            key=f"stream-processor",
+            mode=WebRtcMode.RECVONLY,
+            video_frame_callback=processor,
+            source_video_track=raw_stream.output_video_track,
+            desired_playing_state=raw_stream.state.playing,
+            rtc_configuration=ICE_CONFIG,
+            media_stream_constraints={"video": True, "audio": False},
+        )
+        
 
 
 ICE_CONFIG = {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
@@ -16,16 +65,12 @@ DESIRED_PLAYING_STATE = True
 
 
 def identity(frame: av.VideoFrame) -> av.VideoFrame:
-    event.wait()
-    event.clear()
     return frame
 
 def convert2gray(frame: av.VideoFrame) -> av.VideoFrame:
     image = frame.to_ndarray(format="bgr24")
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     time.sleep(1)
-    # event.set()
-    # print(event)
     return av.VideoFrame.from_ndarray(gray, format="gray")
         
         
@@ -39,7 +84,10 @@ def main():
     method = st.sidebar.radio('Select stream', options=['Webcam', 'File'])
     processor_name = st.sidebar.selectbox("Select Processors:", list(available_processors.keys())) 
     processor = available_processors[processor_name]
-    col_reference, col_processor = st.columns(2)
+    ref_processor = identity # Tu bedzie mozliwosc ustawienia z zewnatrz
+    is_sync = st.sidebar.checkbox("Synchronize processors")
+    if is_sync:
+        ref_processor, processor = synchronize_processors(ref_processor, processor)
     is_stream = False
     if method == "File":
         video_file = st.sidebar.file_uploader("Upload a video file", type=["mp4", "avi"])
@@ -55,26 +103,15 @@ def main():
         source_video_track = None
         is_stream = True
     if is_stream:
-        with col_reference:
-            ctx = webrtc_streamer(
-                    key="stream-org",
-                    mode=mode,
-                    source_video_track=source_video_track,
-                    desired_playing_state=DESIRED_PLAYING_STATE,
-                    media_stream_constraints={"video": True, "audio": False},
-                    rtc_configuration=ICE_CONFIG,
-                    )
-        with col_processor:
-            webrtc_streamer(
-                key=f"stream-{processor_name}",
-                mode=WebRtcMode.RECVONLY,
-                video_frame_callback=processor,
-                source_video_track=ctx.output_video_track,
-                desired_playing_state=ctx.state.playing,
-                rtc_configuration=ICE_CONFIG,
-                media_stream_constraints={"video": True, "audio": False},
-            )
-
+        raw_stream = make_raw_stream(
+            mode=mode,
+            source_video_track=source_video_track,
+        )
+        make_processors_view(
+            raw_stream=raw_stream,
+            ref_processor=ref_processor,
+            processor=processor
+        )
 
 # Run the application
 if __name__ == "__main__":
