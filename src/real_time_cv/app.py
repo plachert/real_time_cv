6cae523dd302aa08c743a8e1cb8bc64421d9f3a1
plachert <pmlachert@gmail.com>
2023-07-07 09:42:18 +0200

    Refactor, synchronize processors with event

--- src/real_time_cv/app.py
+++ src/real_time_cv/app.py
@@ -5,6 +5,10 @@ import tempfile
 import av
 import cv2
 import time
+import threading
+
+event = threading.Event()
+
 
 
 ICE_CONFIG = {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
@@ -12,13 +16,16 @@ DESIRED_PLAYING_STATE = True
 
 
 def identity(frame: av.VideoFrame) -> av.VideoFrame:
+    event.wait()
+    event.clear()
     return frame
 
 def convert2gray(frame: av.VideoFrame) -> av.VideoFrame:
-    
     image = frame.to_ndarray(format="bgr24")
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     time.sleep(1)
+    # event.set()
+    # print(event)
     return av.VideoFrame.from_ndarray(gray, format="gray")
         
         
@@ -30,41 +37,43 @@ available_processors = {
 
 def main():
     method = st.sidebar.radio('Select stream', options=['Webcam', 'File'])
-    processor_names = st.sidebar.multiselect("Select Processors:", list(available_processors.keys())) 
-    
+    processor_name = st.sidebar.selectbox("Select Processors:", list(available_processors.keys())) 
+    processor = available_processors[processor_name]
+    col_reference, col_processor = st.columns(2)
+    is_stream = False
     if method == "File":
         video_file = st.sidebar.file_uploader("Upload a video file", type=["mp4", "avi"])
-        temp_file = tempfile.NamedTemporaryFile(delete=False)
-        temp_file.write(video_file.read())
-        temp_file.close()
-        video_track = FromFileVideoStreamTrack(temp_file.name)
-        ctx = webrtc_streamer(
-            key="stream-org",
-            mode=WebRtcMode.RECVONLY,
-            source_video_track=video_track,
-            desired_playing_state=DESIRED_PLAYING_STATE,
-            media_stream_constraints={"video": True, "audio": False},
-            rtc_configuration=ICE_CONFIG,
-            )
+        if video_file:
+            temp_file = tempfile.NamedTemporaryFile(delete=False)
+            temp_file.write(video_file.read())
+            temp_file.close()
+            source_video_track = FromFileVideoStreamTrack(temp_file.name)
+            mode = WebRtcMode.RECVONLY
+            is_stream = True
     else:
-        ctx = webrtc_streamer(
-            key="stream-org",
-            source_video_track=None,
-            desired_playing_state=DESIRED_PLAYING_STATE,
-            media_stream_constraints={"video": True, "audio": False},
-            rtc_configuration=ICE_CONFIG,
+        mode = WebRtcMode.SENDRECV
+        source_video_track = None
+        is_stream = True
+    if is_stream:
+        with col_reference:
+            ctx = webrtc_streamer(
+                    key="stream-org",
+                    mode=mode,
+                    source_video_track=source_video_track,
+                    desired_playing_state=DESIRED_PLAYING_STATE,
+                    media_stream_constraints={"video": True, "audio": False},
+                    rtc_configuration=ICE_CONFIG,
+                    )
+        with col_processor:
+            webrtc_streamer(
+                key=f"stream-{processor_name}",
+                mode=WebRtcMode.RECVONLY,
+                video_frame_callback=processor,
+                source_video_track=ctx.output_video_track,
+                desired_playing_state=ctx.state.playing,
+                rtc_configuration=ICE_CONFIG,
+                media_stream_constraints={"video": True, "audio": False},
             )
-    for i, processor_name in enumerate(processor_names):
-        processor = available_processors[processor_name]
-        webrtc_streamer(
-            key=f"stream-{processor_name}",
-            mode=WebRtcMode.RECVONLY,
-            video_frame_callback=processor,
-            source_video_track=ctx.output_video_track,
-            desired_playing_state=ctx.state.playing,
-            rtc_configuration=ICE_CONFIG,
-            media_stream_constraints={"video": True, "audio": False},
-        )
 
 
 # Run the application
