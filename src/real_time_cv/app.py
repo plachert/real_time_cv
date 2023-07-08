e8e7213fc082451d31266bbfaccb60868b9b5a29
plachert <pmlachert@gmail.com>
2023-07-08 19:53:05 +0200

    Replace FromFileVideoStreamTrack with MediaPlayer, refactor

--- src/real_time_cv/app.py
+++ src/real_time_cv/app.py
@@ -1,27 +1,30 @@
 from __future__ import annotations
 
 import tempfile
+from functools import partial
 
 import streamlit as st
+from aiortc.contrib.media import MediaPlayer
 from streamlit_webrtc import webrtc_streamer
 from streamlit_webrtc import WebRtcMode
 
 from .processing import dummy_plugin
 from .processing import ProcessorPlugin
 from .processing import synchronize_processors
-from .stream import FromFileVideoStreamTrack
+# from .stream import FromFileVideoStreamTrack
 
 ICE_CONFIG = {'iceServers': [{'urls': ['stun:stun.l.google.com:19302']}]}
-DESIRED_PLAYING_STATE = True
+DESIRED_PLAYING_STATE = None
 
 
-def make_raw_stream(mode, source_video_track):
+def make_raw_stream(mode, source_video_track, player_factory=None):
     with st.sidebar:
         ctx = webrtc_streamer(
             key='stream-org',
             mode=mode,
             source_video_track=source_video_track,
             desired_playing_state=DESIRED_PLAYING_STATE,
+            player_factory=player_factory,
             media_stream_constraints={'video': True, 'audio': False},
             rtc_configuration=ICE_CONFIG,
             video_html_attrs={'hidden': True},
@@ -56,7 +59,10 @@ def make_processors_view(raw_stream, ref_processor, processor):
 def run(processor_plugin: ProcessorPlugin):
     ref_processor = processor_plugin.ref_processor
     available_processors = processor_plugin.available_processors
-    method = st.sidebar.radio('Select stream', options=['Webcam', 'File'])
+    method = st.sidebar.radio(
+        'Select stream',
+        options=['File', 'Webcam'],
+    )
     processor_name = st.sidebar.selectbox(
         'Select Processors:', list(available_processors.keys()),
     )
@@ -72,21 +78,25 @@ def run(processor_plugin: ProcessorPlugin):
         video_file = st.sidebar.file_uploader(
             'Upload a video file', type=['mp4', 'avi'],
         )
+        filename = None
         if video_file:
             temp_file = tempfile.NamedTemporaryFile(delete=False)
-            temp_file.write(video_file.read())
-            temp_file.close()
-            source_video_track = FromFileVideoStreamTrack(temp_file.name)
-            mode = WebRtcMode.RECVONLY
+            temp_file.write(video_file.getbuffer())
             is_stream = True
+            filename = temp_file.name
+        source_video_track = None
+        player_factory = partial(MediaPlayer, filename)
+        mode = WebRtcMode.RECVONLY
     else:
         mode = WebRtcMode.SENDRECV
         source_video_track = None
+        player_factory = None
         is_stream = True
     if is_stream:
         raw_stream = make_raw_stream(
             mode=mode,
             source_video_track=source_video_track,
+            player_factory=player_factory,
         )
         make_processors_view(
             raw_stream=raw_stream,
--- src/real_time_cv/stream.py
+++ src/real_time_cv/stream.py
@@ -1,28 +1,29 @@
 """This module implements streams that can be used with webrtc_streamer."""
 from __future__ import annotations
 
-import cv2
 from aiortc.mediastreams import VideoStreamTrack
-from av import VideoFrame
 
 
-class FromFileVideoStreamTrack(VideoStreamTrack):
-    """
-    Generate frames from video file.
-    """
+class FromImagesStreamTrack(VideoStreamTrack):
+    pass
 
-    def __init__(self, filename):
-        super().__init__()
-        self.filename = filename
-        self.cap = cv2.VideoCapture(filename)
+# class FromFileVideoStreamTrack(VideoStreamTrack):
+#     """
+#     Generate frames from video file.
+#     """
 
-    async def recv(self):
-        pts, time_base = await self.next_timestamp()
-        _, frame = self.cap.read()
-        if frame is None:
-            self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)  # Reset frame position
-            _, frame = self.cap.read()
-        frame = VideoFrame.from_ndarray(frame, 'bgr24')
-        frame.pts = pts
-        frame.time_base = time_base
-        return frame
+#     def __init__(self, filename):
+#         super().__init__()
+#         self.filename = filename
+#         self.cap = cv2.VideoCapture(filename)
+
+#     async def recv(self):
+#         pts, time_base = await self.next_timestamp()
+#         _, frame = self.cap.read()
+#         if frame is None:
+#             self.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)  # Reset frame position
+#             _, frame = self.cap.read()
+#         frame = VideoFrame.from_ndarray(frame, 'bgr24')
+#         frame.pts = pts
+#         frame.time_base = time_base
+#         return frame
